9a26a385e2bccce40ff9c2dfd70ec33b7a9d7037
Brama Udi <bramaudi.al@gmail.com>
2019-08-14 14:30:37 +0700

    Create sql dump

---
+++ udicms.sql
@@ -0,0 +1,218 @@
+-- MySQL dump 10.17  Distrib 10.3.15-MariaDB, for Linux (x86_64)
+--
+-- Host: localhost    Database: kumbara
+-- ------------------------------------------------------
+-- Server version	10.3.15-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `tb_categories`
+--
+
+DROP TABLE IF EXISTS `tb_categories`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tb_categories` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `slug` varchar(20) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `status` int(1) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `slug` (`slug`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_categories`
+--
+
+LOCK TABLES `tb_categories` WRITE;
+/*!40000 ALTER TABLE `tb_categories` DISABLE KEYS */;
+INSERT INTO `tb_categories` VALUES (1,'Uncategorized','Uncategorized post are placed here.','uncategorized',1);
+/*!40000 ALTER TABLE `tb_categories` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tb_gallery`
+--
+
+DROP TABLE IF EXISTS `tb_gallery`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tb_gallery` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `album_id` int(11) NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_gallery`
+--
+
+LOCK TABLES `tb_gallery` WRITE;
+/*!40000 ALTER TABLE `tb_gallery` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tb_gallery` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tb_mod`
+--
+
+DROP TABLE IF EXISTS `tb_mod`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tb_mod` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `post_create` int(1) DEFAULT NULL,
+  `post_edit` int(1) DEFAULT NULL,
+  `post_delete` int(1) DEFAULT NULL,
+  `comment_edit` int(1) DEFAULT NULL,
+  `comment_delete` int(1) DEFAULT NULL,
+  `comment_approve` int(1) DEFAULT NULL,
+  `category_create` int(1) DEFAULT NULL,
+  `category_edit` int(1) DEFAULT NULL,
+  `category_delete` int(1) DEFAULT NULL,
+  `file_create` int(1) DEFAULT NULL,
+  `file_edit` int(1) DEFAULT NULL,
+  `file_delete` int(1) DEFAULT NULL,
+  `gallery_create` int(1) DEFAULT NULL,
+  `gallery_edit` int(1) DEFAULT NULL,
+  `gallery_delete` int(1) DEFAULT NULL,
+  `user_create` int(1) DEFAULT NULL,
+  `user_edit` int(1) DEFAULT NULL,
+  `user_delete` int(1) DEFAULT NULL,
+  `setting` int(1) DEFAULT NULL,
+  `mod` int(1) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_mod`
+--
+
+LOCK TABLES `tb_mod` WRITE;
+/*!40000 ALTER TABLE `tb_mod` DISABLE KEYS */;
+INSERT INTO `tb_mod` VALUES (1,'Admin','Admin is number one',1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1),(2,'Guest','Just give a comments',0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0);
+/*!40000 ALTER TABLE `tb_mod` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tb_posts`
+--
+
+DROP TABLE IF EXISTS `tb_posts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tb_posts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(160) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `slug` varchar(160) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_time` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_time` timestamp NULL DEFAULT NULL,
+  `text` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `category_id` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tags` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `status` int(1) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `slug` (`slug`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_posts`
+--
+
+LOCK TABLES `tb_posts` WRITE;
+/*!40000 ALTER TABLE `tb_posts` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tb_posts` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tb_settings`
+--
+
+DROP TABLE IF EXISTS `tb_settings`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tb_settings` (
+  `id` int(1) NOT NULL AUTO_INCREMENT,
+  `blog_name` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `keywords` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `per_page` int(11) NOT NULL,
+  `comment_moderation` int(1) NOT NULL,
+  `open_register` int(1) NOT NULL,
+  `open_comment` int(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_settings`
+--
+
+LOCK TABLES `tb_settings` WRITE;
+/*!40000 ALTER TABLE `tb_settings` DISABLE KEYS */;
+INSERT INTO `tb_settings` VALUES (1,'UdiCMS','udicms,blog,cms,php','Just Another Blog',5,0,0,1);
+/*!40000 ALTER TABLE `tb_settings` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tb_users`
+--
+
+DROP TABLE IF EXISTS `tb_users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tb_users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `name` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `logged` int(11) NOT NULL,
+  `online` int(11) NOT NULL,
+  `mod` int(11) DEFAULT NULL,
+  `register_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `ban` int(1) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_users`
+--
+
+LOCK TABLES `tb_users` WRITE;
+/*!40000 ALTER TABLE `tb_users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tb_users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-08-14 14:30:16
